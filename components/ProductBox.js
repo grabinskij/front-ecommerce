8a8f0088720be09531145f1eaaf7ebf740c25298
Bogdan Grabinskij <admin@example.com>
2023-12-31 19:19:12 +0100

    Create search page

--- components/ProductBox.js
+++ components/ProductBox.js
@@ -6,7 +6,11 @@ import FlyingButton from "./FlyingButton";
 
 
 const ProductWrapper = styled.div`
-  
+  button{
+    width: 100%;
+    text-align: center;
+    justify-content: center;
+  }
 `;
 const WhiteBox = styled(Link)`
   background-color: #fff;
@@ -35,15 +39,25 @@ const ProductInfoBox = styled.div`
     margin-top: 5px;
 `;
 const PriceRow = styled.div`
-  display: flex;
+  display: block;
+  @media screen and (min-width: 768px) {
+    display: flex;
+    gap: 5px;
+  }
   align-items: center;
-  justify-content: space-between;
-  margin-top: 2px;
+  justify-content:space-between;
+  margin-top:2px;
 `;
 
 const Price = styled.div`
-  font-size: 1.5rem;
-  font-weight: bold;
+  font-size: 1rem;
+  font-weight:400;
+  text-align: right;
+  @media screen and (min-width: 768px) {
+    font-size: 1.2rem;
+    font-weight:600;
+    text-align: left;
+  }
 `;
 export default function ProductBox({_id, title, description, price, images}){
     const {addProduct} = useContext(CartContext);
