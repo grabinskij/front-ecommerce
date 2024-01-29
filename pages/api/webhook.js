181d37f6cde154323c8d7fbfe1d899db1473849e
Bogdan Grabinskij <admin@example.com>
2024-01-29 17:52:16 +0100

    Check payment_status

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -24,6 +24,7 @@ export default async function handler(req,res) {
             const data = event.data.object;
             const orderId = data.metadata.orderId;
             const paid = data.payment_status === 'paid';
+            console.log(data, data.payment_status);
             if(orderId && paid){
                await Order.findByIdAndUpdate(orderId, {
                     paid: true,
@@ -33,6 +34,8 @@ export default async function handler(req,res) {
         default:
             console.log(`Unhandled event type ${event.type}`);
     }
+
+
     res.status(200).send('ok');
 }
 
